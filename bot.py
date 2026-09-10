0e8b295c53cec343561e8241458e603db9a4a6bf
Shaxboz1997 <shaxboz1997vdx@gmail.com>
2026-09-10 10:59:58 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,32 @@
+import os
+import asyncio
+from aiogram import Bot, Dispatcher
+from aiogram.filters import CommandStart
+from aiogram.types import Message
 
+TOKEN = os.getenv("BOT_TOKEN")
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+
+@dp.message(CommandStart())
+async def start(message: Message):
+    await message.answer(
+        "🇺🇿 Assalomu alaykum!\n\n"
+        "🎓 IMA Prezident School AI Bot'iga xush kelibsiz!\n\n"
+        "Bu bot sizni 8 oy davomida Prezident maktabi imtihoniga "
+        "bosqichma-bosqich tayyorlaydi.\n\n"
+        "🇬🇧 English\n"
+        "➗ Matematika\n"
+        "🧠 Mantiq\n\n"
+        "🤖 AI sizning natijalaringizni tahlil qiladi."
+    )
+
+
+async def main():
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
